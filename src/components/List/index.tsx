66ab9944c4b95564b7dada16156d510aa20ac727
NycolasCruz <nycolascruzsilva@gmail.com>
2024-05-29 13:27:05 -0300

    feat: new components

---
+++ src/components/List/index.tsx
@@ -0,0 +1,9 @@
+export function List() {
+	return (
+		<ul>
+			<li>item 1</li>
+			<li>item 2</li>
+			<li>item 3</li>
+		</ul>
+	);
+}
---
+++ src/components/NewItem/index.tsx
@@ -0,0 +1,13 @@
+export function NewItem() {
+	return (
+		<>
+			<label htmlFor="new-item">Adicione um novo item à lista</label>
+
+			<div>
+				<input id="new-item" />
+			</div>
+
+			<button>Adicionar</button>
+		</>
+	);
+}
