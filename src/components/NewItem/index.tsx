aed9837a659e3452e2637a15b6ab72c4abb97388
NycolasCruz <nycolascruzsilva@gmail.com>
2024-05-29 16:42:20 -0300

    feat: new components

--- src/components/NewItem/index.tsx
+++ src/components/NewItem/index.tsx
@@ -1,13 +1,31 @@
-export function NewItem() {
+import { FormEvent, useState } from "react";
+
+type Props = {
+	list: string[];
+	setList: (list: string[]) => void;
+};
+
+export function NewItem({ list, setList }: Props) {
+	const [value, setValue] = useState("");
+
+	function handleAddingTheItemToTheList(event: FormEvent) {
+		event.preventDefault();
+
+		setList([...list, value]);
+		setValue("");
+	}
+
 	return (
 		<>
 			<label htmlFor="new-item">Adicione um novo item à lista</label>
 
-			<div>
-				<input id="new-item" />
-			</div>
+			<form style={{ display: "flex", gap: "0.5rem", marginTop: "0.5rem" }}>
+				<input id="new-item" value={value} onChange={(event) => setValue(event.target.value)} />
 
-			<button>Adicionar</button>
+				<button disabled={!value} onClick={(event) => handleAddingTheItemToTheList(event)}>
+					Adicionar
+				</button>
+			</form>
 		</>
 	);
 }
